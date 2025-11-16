1bdc1d08262cd61e908ad2449e65ec618c5e4b2b
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-11-16 17:45:03 +0530

    feat: initialize app routes and create JSX pages for navigation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,20 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css'
-import App from './App.jsx'
+import Login from './Views/Login';
+import Signup from './Views/Signup';
+import Home from './Views/Home';
+import About from './Views/About';
+import Cart from './Views/Cart';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
+    </BrowserRouter>
 )

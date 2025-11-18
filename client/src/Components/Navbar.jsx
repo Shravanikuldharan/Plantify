8ab6ae477a4b0a2c789573195e572c1b3eeb2ac6
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-11-18 18:45:45 +0530

    Favicon added

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router';
-import { FiUser  } from "react-icons/fi"; // ← icon import
+import { FiUser  } from "react-icons/fi";
+import logo from "../assets/logo.png";
 
 function Navbar() {
 
@@ -8,16 +9,17 @@ function Navbar() {
     <nav className="bg-orange-100 shadow">
       <div className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
 
-        {/* Logo */}
-        <h1 className="text-xl font-bold text-green-700">Plantify</h1>
+         <Link to="/" className="flex items-center space-x-2">
+          <img src={logo} className="h-12 h-12 object-contain" alt="Plantify Logo" />
+          <h1 className="text-2xl font-bold text-green-700">Plantify</h1>
+        </Link>
 
-        {/* Links */}
         <div className="flex space-x-6 text-gray-700 font-medium">
           <Link to="/" className="hover:text-green-600">Home</Link>
           <Link to="/plants" className="hover:text-green-600">Plants</Link>
           {/* <Link to="/about" className="hover:text-green-600">About</Link> */}
           <Link to="/cart" className="hover:text-green-600">Cart</Link>
- <Link to="/login" className="text-gray-700 hover:text-green-600 text-xl">
+          <Link to="/login" className="text-gray-700 hover:text-green-600 text-xl">
           <FiUser  />
         </Link>
           {/* <Link to="/signup" className="hover:text-green-600">Signup</Link> */}
